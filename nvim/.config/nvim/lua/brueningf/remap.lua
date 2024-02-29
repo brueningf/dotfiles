42189fa1b264705f4dbb825b4b2434c40f812cee
FredericBruening <contact@fredesk.com>
2024-02-29 13:58:10 -0500

    Add leader x remap for chmod +x

--- nvim/.config/nvim/lua/brueningf/remap.lua
+++ nvim/.config/nvim/lua/brueningf/remap.lua
@@ -1,14 +1,20 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 
+-- swift tem
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- center page
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+-- center next
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- paste from clipb
 vim.keymap.set("n", "<leader>p", "\"_dP")
 
+-- execute it
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
