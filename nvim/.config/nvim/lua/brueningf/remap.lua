fe1494b8c5b2479823fcc5e6e934bc516cd735d2
FredericBruening <contact@fredesk.com>
2024-03-09 15:19:42 -0500

    add parser to treesitter, remap

--- nvim/.config/nvim/lua/brueningf/remap.lua
+++ nvim/.config/nvim/lua/brueningf/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
 
--- swift tem
+-- slide line
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
